7bb8c9e091ad40bd1c0ddeca2e717bf5b9e9bec8
Artur-Panasiuk <47883583+Artur-Panasiuk@users.noreply.github.com>
2022-10-21 23:37:06 +0200

    class structure

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class dynArr{
+    double arrayMultip {2};
+    int currSize {0};
+    int maxSize {1};
+    T* arr;
+
+    dynArr(){
+
+    }
+    ~dynArr(){
+
+    }
+public:
+    void add(T data){
+
+    }
+    void get(int index){
+
+    }
+    void swapData(int index, T data){
+
+    }
+    void clean(){
+
+    }
+    string toString(){
+
+    }
+    void bubbleSort(){
+
+    }
+};
+
+int main()
+{
+
+
+    return 0;
+}
