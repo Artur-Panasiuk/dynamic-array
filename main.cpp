8d4636022e3c189232c45d7c6b9ba76fc22910dc
Artur-Panasiuk <47883583+Artur-Panasiuk@users.noreply.github.com>
2022-10-23 01:13:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,31 +8,61 @@ class dynArr{
     int currSize {0};
     int maxSize {1};
     T* arr;
+    T* tempArr;
 
     dynArr(){
-
+        arr = new T[maxSize];
     }
     ~dynArr(){
-
+        clean();
+        delete arr;
     }
 public:
     void add(T data){
-
+        if(currSize == maxSize){
+            tempArr = new T[currSize];
+            for(int i = 0; i < currSize; i++){
+                tempArr[i] = arr[i];
+            }
+            delete arr;
+            maxSize  *= arrayMultip;
+            arr = new T[maxSize];
+            for(int j = 0; j < currSize; j++){
+                arr[j] = temp[j];
+            }
+            delete temp;
+        }else{
+            arr[currSize + 1] = data;
+        }
     }
-    void get(int index){
+    void get(int index = 0){
+        if(index < 0 || index > maxSize) return;
 
+        return arr[index];
     }
     void swapData(int index, T data){
-
+        if(index < 0 || index > maxSize) return;
     }
-    void clean(){
-
+    void clean(bool isObjectType){
+        if(isObjectType){
+            for(int i = 0; i < currSize; i++){
+                delete arr[i];
+            }
+        }else{
+            for(int j = 0; j < currSize; j++){
+                arr[i] = NULL;
+            }
+        }
     }
     string toString(){
 
     }
-    void bubbleSort(){
+    void bubbleSort(bool isObjectType){
+        if(isObjectType){
 
+        }else{
+
+        }
     }
 };
 
