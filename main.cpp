848f07e549c7b9e4ac12fd6145cf2d53a45b5e21
Artur-Panasiuk <47883583+Artur-Panasiuk@users.noreply.github.com>
2022-10-23 15:20:47 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
@@ -9,15 +10,14 @@ class dynArr{
     int maxSize {1};
     T* arr;
     T* tempArr;
-
+public:
     dynArr(){
         arr = new T[maxSize];
     }
     ~dynArr(){
-        clean();
+        //clean(false);
         delete arr;
     }
-public:
     void add(T data){
         if(currSize == maxSize){
             tempArr = new T[currSize];
@@ -28,12 +28,12 @@ public:
             maxSize  *= arrayMultip;
             arr = new T[maxSize];
             for(int j = 0; j < currSize; j++){
-                arr[j] = temp[j];
+                arr[j] = tempArr[j];
             }
-            delete temp;
-        }else{
-            arr[currSize + 1] = data;
+            delete tempArr;
         }
+            arr[currSize] = data;
+            currSize++;
     }
     void get(int index = 0){
         if(index < 0 || index > maxSize) return;
@@ -43,32 +43,48 @@ public:
     void swapData(int index, T data){
         if(index < 0 || index > maxSize) return;
     }
-    void clean(bool isObjectType){
-        if(isObjectType){
-            for(int i = 0; i < currSize; i++){
-                delete arr[i];
+    void clean(bool isPOD = false){
+        if(isPOD){
+            for(int j = 0; j < currSize; j++){
+                arr[j] = NULL;
             }
         }else{
-            for(int j = 0; j < currSize; j++){
-                arr[i] = NULL;
-            }
+            return;
         }
     }
-    string toString(){
-
+    void toString(){
+        for(int i = 0; i < currSize; i++){
+            cout<< i << "-" << arr[i]<<endl;
+        }
+        cout<<"Max size: "<<maxSize<<endl;
     }
-    void bubbleSort(bool isObjectType){
-        if(isObjectType){
+    void bubbleSort(bool isPOD = false){
+        if(isPOD){
 
         }else{
-
+            return;
         }
     }
 };
 
+void printString(string text){
+
+}
+
+void timeTest(){
+
+}
+
 int main()
 {
+    dynArr<int>* a = new dynArr<int>;
 
+    for(int i = 0; i < 100; i++){
+        a->add(i);
+    }
+    a->toString();
+
+    delete a;
 
     return 0;
 }
