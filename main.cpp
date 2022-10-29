9af15803d25200dc69f98b3d5f482e7c9aa27704
Artur-Panasiuk <47883583+Artur-Panasiuk@users.noreply.github.com>
2022-10-29 17:08:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -87,9 +87,14 @@ public:
 
     void bubbleSort(bool isPOD = false) {
         if (isPOD) {
-
-        }
-        else {
+            for (int i = 0; i < n - 1; i++){
+                for (int j = 0; j < n - i - 1; j++){
+                    if (arr[j] > arr[j + 1]){
+                       swap(arr[j], arr[j + 1]);
+                    }
+                }
+            }
+        } else {
             return;
         }
     }
